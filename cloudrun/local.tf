2fc88a317893b5234b17a56413fce58819e7bab3
kazshi01 <blumedio0619@gmail.com>
2025-03-11 21:36:07 +0900

    元に戻し

--- cloudrun/local.tf
+++ cloudrun/local.tf
@@ -1,9 +1,4 @@
 locals {
   project_id = "encoded-copilot-453019-e7"
-  project_number = "421883982947"
   region = "asia-northeast1"
-
-  github_app_installation_id = "62425340"
-  github_oauth_token_secret_version = "projects/421883982947/secrets/github-host-github-oauthtoken-fdd045/versions/1"
-  github_repository_remote_uri = "https://github.com/kazshi01/gcp.git"
 }
